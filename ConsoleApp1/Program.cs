be13910482a7d7a401cf51fef31ceed5ebb137cb
jguiapal <jguiapal@newbrighton.edu.ph>
2024-01-22 14:50:28 +0800

    simple calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace ConsoleApp1
+﻿namespace CalculatorTraining
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double firstNum;
+            double secondNum;
+            double operation;
+            double answer;
+
+            Console.WriteLine("This is a simple Calculator");
+            Console.WriteLine("Enter First Number:");
+            firstNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            secondNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an operation \r\n1. Addition\r\n2. Subtraction\r\n3. Multiplication\r\n4. Division\r\nEnter Number: ");
+            operation = Convert.ToDouble(Console.ReadLine());
+
+            if (operation == 1) { answer = firstNum + secondNum; }
+            else if (operation == 2) { answer = firstNum - secondNum; }
+            else if (operation == 3) { answer = firstNum * secondNum; }
+            else { answer = firstNum / secondNum; };
+
+
+
+            Console.WriteLine("The answer is " + answer);
+
         }
     }
 }
